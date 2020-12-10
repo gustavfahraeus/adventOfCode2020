e8fbf3aec75d91a829972d713ff4c96ce7b0dbbf
Gustav Fåhraeus <g.fahraeus@hotmail.com>
2020-12-10 19:56:12 +0100

    day8

--- day7.py
+++ day7.py
@@ -4,13 +4,6 @@ file = open("input.txt", 'r')
 
 input = file.read().splitlines()
 
-
-
-
-
-
-
-
 ruleBook = {}
 for rule in input:
     typeOfBag = rule.split(' contain ')[0].replace('bags', 'bag')
@@ -19,15 +12,24 @@ for rule in input:
     bagRestrictions = list(map(lambda string: string.replace('bags', 'bag'), bagRestrictions))
     
     for restriction in bagRestrictions:
-        if restriction.split(" ", 1)[0] == "no": ruleBook[typeOfBag] = 0
+        if restriction.split(" ", 1)[0] == "no": continue
         else: ruleBook[typeOfBag] = {restriction.split(" ", 1)[1]: int(restriction.split(" ", 1)[0])}
+           
 
+visited = []
+sum = 0
+def bagPresent(inputBag):
+    sum = 0
+    for outerBag, innerBags in ruleBook.items():
+        for bag in innerBags:
+            if bag == inputBag and not (outerBag in visited):
+                visited.append(outerBag)
+                sum += 1 + bagPresent(outerBag) 
+                    
+    return sum
 
-
-print(ruleBook)
-
-
-
+            
+print(bagPresent("shiny gold bag"))
 
 
 
